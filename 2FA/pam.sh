8bb939db69b8efbf33420530df1c32ee594a5483
Milad.Naebzadeh <45167308+naebzadehmilad@users.noreply.github.com>
2024-12-05 03:28:17 +0330

    Create pam.sh

---
+++ 2FA/pam.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USER_SCRIPT="/home/$PAM_USER/sms.sh"
+
+if [ -f "$USER_SCRIPT" ]; then
+    exec "$USER_SCRIPT"
+else
+    echo "Script for user $PAM_USER not found at $USER_SCRIPT" >&2
+    exit 1
+fi
